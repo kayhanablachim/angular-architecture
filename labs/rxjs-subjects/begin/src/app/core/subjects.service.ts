c2ef3ae17b584a5462861f3e5990f3a2f2bb8b68
Kayhan <mert-kayhan.ablachim@ortec.com>
2021-11-16 19:31:38 +0100

    Working with RxJS Subjects

--- labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
+++ labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsService {
-  
-  //Define Subject and Observable properties here
 
+  //Define Subject and Observable properties here
+  subject$: BehaviorSubject<string>;
+  observable$: Observable<string>;
 
   constructor() {
     this.init();
   }
 
   init() {
-    // Create Subject and Observable Here 
-
+    // Create Subject and Observable Here
+    this.subject$ = new BehaviorSubject(null);
+    this.observable$ = this.subject$.asObservable();
 
     // Create interval here
+    setInterval(() => {
+      const date = new Date();
 
+      // Place the following code on a single line when adding it
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      this.subject$.next(time);
+    }, 3000)
   }
 
 }
